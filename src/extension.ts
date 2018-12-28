03f30ba741e424cc44e0e40b967c4e248da94e87
Daniel Braunwarth <d4nuu8@gmail.com>
2018-12-28 13:03:43 +0100

    Implement tslint suggestions

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,17 @@
-'use strict';
-// The module 'vscode' contains the VS Code extensibility API
+"use strict";
+// The module "vscode" contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    vscode.languages.setLanguageConfiguration('krl', {
+    vscode.languages.setLanguageConfiguration("krl", {
         indentationRules: {
-            increaseIndentPattern: /^\s*(FOR.*|IF.*|ELSE|LOOP|REPEAT|WHILE.*|SWITCH.*|CASE.*|DEFAULT.*)\s*(;.*)?$/i,
-            decreaseIndentPattern: /^\s*(ENDFOR|ELSE|ENDIF|ENDLOOP|UNTIL.*|ENDWHILE|ENDSWITCH|CASE.*|DEFAULT.*)\s*(;.*)?$/i
-        }
-    })
-}
-
-// this method is called when your extension is deactivated
-export function deactivate() {
+            decreaseIndentPattern: new RegExp(
+                /^\s*(ENDFOR|ELSE|ENDIF|ENDLOOP|UNTIL.*|ENDWHILE|ENDSWITCH|CASE.*|DEFAULT.*)\s*(;.*)?$/, "i"),
+            increaseIndentPattern: new RegExp(
+                /^\s*(FOR.*|IF.*|ELSE|LOOP|REPEAT|WHILE.*|SWITCH.*|CASE.*|DEFAULT.*)\s*(;.*)?$/, "i"),
+        },
+    });
 }
